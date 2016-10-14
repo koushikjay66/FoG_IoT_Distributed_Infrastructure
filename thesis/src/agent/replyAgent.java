b77b982cacc18653cd7dc1daaec39602516bcb9a
Arshad <arshad.antu@gmail.com>
2016-10-14 15:05:56 +0600

    replyAgent builder class edited

--- thesis/src/agent/replyAgent.java
+++ thesis/src/agent/replyAgent.java
@@ -12,112 +12,93 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import service.Service;
+import serviceCompositionSoa.M2MReply;
+
 /**
  *
  * @author arsha
  */
 public class replyAgent {
-    
-    public static Map<String,String> valuemap = new HashMap();
-    public Object[] serviceResult; 
+
+    public static Map<String, String> valuemap = new HashMap();
+    public Object[] serviceResult;
     private String serviceName;
-    
-    
-    
-    public replyAgent(Service s){
-        this.serviceName=s.name;
+
+    public replyAgent(Service s) {
+        this.serviceName = s.name;
+
         updateAgent ua = new updateAgent();
         getComplexServiceValues(serviceName);
         increaseUpdateCount(serviceName);
         ua.increasePriority(serviceName); // increasePriority
-        
-    }// End of function constructor
-    
-    public void increaseUpdateCount(String serviceName){
-        
-        String sql = "UPDATE complex_service SET cs_update_count = cs_update_count + 1 WHERE cs_name=\""+serviceName+"\"";
-        mysql result = new mysql(sql, "UPDATE", "agent_lookup_table");
-    }
-    /**
-     * 
-     * @param serviceName - the required service name 
-     */
-    // this searches for the complex service
-    public static void getComplexServiceValues(String serviceName){ 
-        String sql = "SELECT ss_name FROM complex_service, simple_service, service_relation "
-                + "WHERE cs_name=\""+serviceName+"\" AND complex_service.csid=service_relation.csid AND "
-                + "service_relation.ssid=simple_service.ssid";
-        
-        mysql result = new mysql(sql, "SELECT", "agent_lookup_table");
-        
-        
-        for (Object i: result.res.keySet()) {
-            String basics = result.res.get(i).toString().substring(1, result.res.get(i).toString().length()-1);
-            
-            if (basics.length()!=0) {
-                System.out.println(result.res.get(i));
-                String [] basicServices = basics.split( ", ");// divide every 
-            
-                for (int j = 0; j < basicServices.length; j++) {
-                    valuemap.put(basicServices[j],getsimpleService(basicServices[j]));
-                }
-            }else{
-                //ask cloud
-                
-                
+
+        serviceResult = new Object[valuemap.size()];
+        int k = 0;
+        if (valuemap.keySet() != null) {
+            for (Object i : valuemap.keySet()) {
+                M2MReply bulala = new M2MReply(i.toString(), valuemap.get(i).toString());
+                serviceResult[k] = bulala;
+                k++;
             }
         }
+    }// End of function constructor
+
+    public void increaseUpdateCount(String serviceName) {
+
+        String sql = "UPDATE complex_service SET cs_update_count = cs_update_count + 1 WHERE cs_name=\"" + serviceName + "\"";
+        mysql result = new mysql(sql, "UPDATE", "agent_lookup_table");
     }
-    
-    //This returns the basic service value as a string "serviceName,Value"
-    public static String getsimpleService(String serviceName){
-        
-        String sql="SELECT ss_name, ss_value FROM simple_service WHERE ss_name =\""+serviceName+"\"";
-        
+
+    /**
+     *
+     * @param serviceName - the required service name
+     */
+    // this searches for the complex service
+    public static void getComplexServiceValues(String serviceName) {
+        String sql = "SELECT ss_name FROM complex_service, simple_service, service_relation "
+                + "WHERE cs_name=\"" + serviceName + "\" AND complex_service.csid=service_relation.csid AND "
+                + "service_relation.ssid=simple_service.ssid";
+
         mysql result = new mysql(sql, "SELECT", "agent_lookup_table");
-        
-        String name="",value="";
-        for (Object i: result.res.keySet()) {
-            if (i.equals("ss_name")) {
-                 name = result.res.get(i).toString().substring(1, result.res.get(i).toString().length()-1);
-//                System.out.println("name: "+name);
-            }
-            else if (i.equals("ss_value")) {
-                 value = result.res.get(i).toString().substring(1, result.res.get(i).toString().length()-1);
-//                System.out.println("value: "+ value);
+
+        if (result.res.get("ss_name").toString().equals("[]")) {
+            valuemap.put(serviceName, getsimpleService(serviceName));
+        } else {
+            for (Object i : result.res.keySet()) {
+                String basics = result.res.get(i).toString().substring(1, result.res.get(i).toString().length() - 1);
+                String[] basicServices = basics.split(", ");// divide every 
+                for (int j = 0; j < basicServices.length; j++) {
+                    valuemap.put(basicServices[j], getsimpleService(basicServices[j]));
+                }
+
+            }// End of for loop 
+
+        }
+    }
+
+    //This returns the basic service value as a string "serviceName,Value"
+    public static String getsimpleService(String serviceName) {
+
+        String sql = "SELECT ss_value FROM simple_service WHERE ss_name =\"" + serviceName + "\"";
+        mysql result = new mysql(sql, "SELECT", "agent_lookup_table");
+
+        String value = "";
+        if (!result.res.get("ss_value").toString().equals("[]")) {
+            for (Object i : result.res.keySet()) {
+                if (i.equals("ss_value")) {
+                    value = result.res.get(i).toString().substring(1, result.res.get(i).toString().length() - 1);
+                }
             }
         }
         return value;
     }// End of method getSimpleService
-    
-    public static double ttlCount(String name,String timestamp){ //need this fixed calculation not working
-        long diff=0;
-        try{
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.S");
-            Date parsedDate = dateFormat.parse(timestamp);
-            Timestamp timestmp = new java.sql.Timestamp(parsedDate.getTime());
-//         System.out.println(timestmp);
-            long st = timestmp.getTime();
-            long ct = System.currentTimeMillis();
-            diff = ct-st;
-//            System.out.println(name+" service time: "+st);
-//            System.out.println(name+" current time: "+ct);
-//            System.out.print(name+" ttl Differance: ");
-//            System.out.println(name+ " " +(diff/1000.0)+ " seconds");
-        }catch(Exception e){
-            System.out.println(name+" Time calculation error");
-        }
-        return (diff/1000.0);
-    }
-    
-    
-    public static void main(String[] args){ // just for checking
+
+    public static void main(String[] args) { // just for checking
         getComplexServiceValues("env");
-        for (Object i: valuemap.keySet()) {
-            
-            System.out.println(i.toString()+"-->"+valuemap.get(i).toString());
+        for (Object i : valuemap.keySet()) {
+
+            System.out.println(i.toString() + "-->" + valuemap.get(i).toString());
         }
     }
-    
-    
+
 }
