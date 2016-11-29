d07030e76fd3c3ebb2de32e03bc8adba12f2feda
koushikjay66 <koushikjay66@gmail.com>
2016-11-29 21:07:36 +0600

    M2M code

---
+++ src/main.java
@@ -0,0 +1,37 @@
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import json.Builder.Builder;
+import json.Builder.objects.M2M_Request;
+import server.Requestinfo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Koushik
+ */
+public class main {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println("Enter SOA IP");
+        Scanner lb = new Scanner(System.in);
+        // String temp_ip =lb.next();
+        String temp_ip = "172.16.34.38";
+        Requestinfo rs = new Requestinfo(temp_ip);
+        
+        M2M_Request req= new M2M_Request("REQ").build("koushikjay66", "Nopassword01", "env", "erfan");
+        
+        String reqJson = Builder.compile(req);
+        
+        
+        System.out.println("Requested\n"+reqJson);
+        
+        rs.sendData(reqJson);
+        System.out.println(rs.getReply());
+    }
+}
